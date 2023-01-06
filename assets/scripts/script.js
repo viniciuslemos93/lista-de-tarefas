0b893135ee455cfe237933bc319779dafb545bb4
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-05 21:31:14 -0300

    Alterado a função de adicionar tarefas, agora com menas duas linhas de código

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,19 @@
-var novaTarefa = window.document.getElementById('nova-tarefa')
-var addTarefa = novaTarefa.value
-var tarefas = window.document.getElementById('container-tarefas')
-
 function inserirNovaTarefa() {
-    validarTarefa();
-}
-function validarTarefa () {
     var novaTarefa = window.document.getElementById('nova-tarefa')
-    var tarefas = window.document.getElementById('container-tarefas')
     var addTarefa = novaTarefa.value
-    if (novaTarefa == "") {
+    var tarefas = window.document.getElementById('container-tarefas')
+
+    if (addTarefa == "") {
         alert("Campo vazio, por favor insira uma Tarefa.")
 
     } else {
-        console.log(addTarefa)
-        tarefas.innerText = `${tarefas}`
+        /* Outra forma de criar uma Tag filha.
+        const item = document.createElement('p')
+        item.innerHTML = `${addTarefa}`
+        document.getElementById('container-tarefas').appendChild(item)
+        */
+
+        tarefas.insertAdjacentHTML('beforeend', `<p class="tarefa">${addTarefa}<p>`);
+
     }
 }
